a6d6d6cfa49146df8599ae2683326f5ebe44eeec
Brian Howard <bhoward@depauw.edu>
2024-08-24 12:38:27 -0400

    Expanded Doodle examples

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -13,12 +13,58 @@ import cats.effect.unsafe.implicits.global
 // 1. run `sbt`
 // 2. run the `run` command within `sbt`
 object Example {
-  val image =
-    Image
-      .circle(100)
-      .fillColor(Color.red)
-      .on(Image.circle(200).fillColor(Color.aquamarine))
-      .on(Image.circle(300).fillColor(Color.steelBlue))
+  val image1 =
+    Image.circle(100).fillColor(Color.red) `on`
+    Image.circle(200).fillColor(Color.aquamarine) `on`
+    Image.circle(300).fillColor(Color.steelBlue)
+  
+  def spiral(dot: Image, radius: Double, step: Angle, samples: Int): Image = {
+    def aux(count: Int): Image = {
+      val r = radius * count / samples
+      val theta = step * count
+      val color = Color.hsl(theta * 5, 1, 0.5)
+      count match
+        case 0 => Image.empty
+        case n => dot.at(r, theta).fillColor(color) `on` aux(n - 1)
+    }
+
+    aux(samples)
+  }
+
+  val image2 = spiral(Image.star(5, 14, 5), 250, 42.degrees, 200)
+
+  def sierpinski(count: Int): Image = {
+    count match
+      case 0 => Image.triangle(10, 10).strokeColor(Color.blue)
+      case n =>
+        val unit = sierpinski(n - 1)
+        unit `above` (unit `beside` unit)
+  }
+
+  val image3 = sierpinski(6)
+
+  def digitFlower(num: Int, radius: Double): Image = {
+    def aux(count: Int, petal: Image, step: Angle): Image = {
+      count match
+        case 0 => Image.empty
+        case n => {
+          val point = Point(radius, step * n)
+          petal.at(point) `on`
+            Image.path(OpenPath.empty.lineTo(point)) `on`
+            aux(count - 1, petal, step)
+        }
+    }
+
+    num match
+      case 0 => Image.empty
+      case n => {
+        val digit = n % 10
+        val petal = digitFlower(n / 10, radius / 3)
+        aux(digit, petal, 1.turns / digit)
+      }
+  }
+
+  val image4 = digitFlower(34567, 200)
 
   val animation =
     Reactor
@@ -28,11 +74,8 @@ object Example {
       .withTickRate(20.millis)
       .withRender { a =>
         val location = Point(200, a)
-        val planet = Image.circle(40.0).noStroke.fillColor(Color.seaGreen)
-        val moon = Image
-          .circle(10.0)
-          .noStroke
-          .fillColor(Color.slateGray)
+        val planet = Image.circle(40).noStroke.fillColor(Color.seaGreen)
+        val moon = Image.circle(10).noStroke.fillColor(Color.slateGray)
           .at(Point(60, a * 5))
 
         moon.on(planet).at(location)
@@ -41,7 +84,10 @@ object Example {
   val frame = Frame.default.withSize(600, 600).withCenterAtOrigin
 
   @main def go(): Unit = {
-    image.draw()
+    // image1.draw()
+    // image2.draw()
+    // image3.draw()
+    image4.draw()
 
     // Comment out the above and uncomment the below to display the animation
     // animation.run(frame)
