1856cd640fd53fe212f00e9ad7f3e273f52fc802
Brian Howard <bhoward@depauw.edu>
2025-03-23 22:25:01 -0400

    Improved LIS output

--- src/main/scala/algorithms/LIS.scala
+++ src/main/scala/algorithms/LIS.scala
@@ -69,22 +69,24 @@ object version1LIS {
   @main
   def lisDemo(): Unit = {
     val input = List(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8)
-    println(version1(input))
-    println(version2(input.toArray))
-    println(version3(input.toArray))
-    println(version4(input.toArray))
+    println(s"v1(12): ${version1(input)}")
+    println(s"v2(12): ${version2(input.toArray)}")
+    println(s"v3(12): ${version3(input.toArray)}")
+    println(s"v4(12): ${version4(input.toArray)}")
 
-    val longInput = Seq.fill(100)(scala.util.Random.nextInt)
-    println(time(version1(longInput.toList)))
-    println(time(version2(longInput.toArray)))
-    println(time(version3(longInput.toArray)))
-    println(time(version4(longInput.toArray)))
+    val n1 = 100
+    val longInput = Seq.fill(n1)(scala.util.Random.nextInt)
+    println(s"time v1($n1): ${time(version1(longInput.toList))}")
+    println(s"time v2($n1): ${time(version2(longInput.toArray))}")
+    println(s"time v3($n1): ${time(version3(longInput.toArray))}")
+    println(s"time v4($n1): ${time(version4(longInput.toArray))}")
 
-    val longInput2 = Seq.fill(120)(scala.util.Random.nextInt)
-    println(time(version1(longInput2.toList)))
-    println(time(version2(longInput2.toArray)))
-    println(time(version3(longInput2.toArray)))
-    println(time(version4(longInput2.toArray)))
+    val n2 = 120
+    val longInput2 = Seq.fill(n2)(scala.util.Random.nextInt)
+    println(s"time v1($n2): ${time(version1(longInput2.toList))}")
+    println(s"time v2($n2): ${time(version2(longInput2.toArray))}")
+    println(s"time v3($n2): ${time(version3(longInput2.toArray))}")
+    println(s"time v4($n2): ${time(version4(longInput2.toArray))}")
   }
 
 
